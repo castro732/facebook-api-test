0aca1af78b96d97bdc44e16c7555f47cda59b9fe
Jose <castro732@gmail.com>
2018-03-14 01:45:20 -0300

    Add MainController

---
+++ app/Http/Controllers/MainController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+
+class MainController extends Controller
+{
+    protected $fb;
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+}
